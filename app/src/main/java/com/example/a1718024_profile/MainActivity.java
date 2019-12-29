aa8e5c9fdeed4f6fe9f0329d59139f706b3e96ab
Yolanda APK <yolandaapriliapk1204@gmail.com>
2019-12-29 11:47:21 +0700

    1718024

--- app/src/main/java/com/example/a1718024_profile/MainActivity.java
+++ app/src/main/java/com/example/a1718024_profile/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.a1718024_profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ImageButton setting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setting = findViewById(R.id.setting);
+        setting.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId() == R.id.setting){
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(intent);
+        }
     }
 }
